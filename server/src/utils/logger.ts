09306b29118cb7016afb867002b8774407ae4d37
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2023-11-16 13:35:38 -0500

    Added file logging to the logger using pino transport

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,4 +1,23 @@
-const logger = require("pino")(require("pino-pretty")());
+const pino = require("pino");
+const toLog = pino.transport({
+  targets: [
+    {
+      level: 'info',
+      target: 'pino/file',
+      options: {
+        destination: `data/app.log`
+      },
+    },
+    {
+      target: 'pino-pretty'
+    }
+  ]
+});
+
+const pretty = require("pino-pretty");
+const logger = pino({}, toLog);
+
+const fs = require("fs/promises");
 
 export const logging = (message: string, vars: unknown = "") => logger.info(`${message} ${vars}`);
 export const errorLog = (message: string, vars: unknown = "") => logger.error(`${message} ${vars}`);
