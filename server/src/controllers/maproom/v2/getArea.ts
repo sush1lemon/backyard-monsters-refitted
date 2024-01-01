fa77c19f6e06b9ae352b18961d7322c3608576a6
React <bymdestroyers@gmail.com>
2024-01-01 21:50:59 +0000

    Added additional documentation to properties in getArea.ts | Added MR2 missing assets

--- server/src/controllers/maproom/v2/getArea.ts
+++ server/src/controllers/maproom/v2/getArea.ts
@@ -15,10 +15,11 @@ export const getArea: KoaController = async (ctx) => {
 
     for (let y = 0; y < maxY; y++) {
       data[x][y] = {
-        // User ID - ctx.session.userid for real player
+        // User ID types 
+        // ctx.session.userid - real player | 0 - Wild Monsters | 0 - Other
         uid: 0,
         // Base types:
-        // Wild Monster: 1
+        // Wild Monster: 1 | Main Yard: 2 | Outpost: 3
         b: 1,
         // Facebook ID
         fbid: "0",
@@ -33,23 +34,25 @@ export const getArea: KoaController = async (ctx) => {
         // Sand: sand1: < 105 | sand2: < 110 
         // Land: land1: < 120 | land2: < 140 | land3: < 160 | land4: < 170
         // Rock: land5: < 175 | else: land6
-        i: 104,
+        i: 109,
         // Owns cell
         mine: 0,
         // Flinger Level
         f: 1,
         // Catapult Level
         c: 1,
-        // Truces
+        // Truce
         t: 0,
         // Base names:
         // Options: username | Dreadnaut | Kozu | Legionnaire | Abunakki
-        n: `Kozu`,
+        n: "Dreadnaut",
         // Friends
         fr: 0,
-        // Is currently online
-        on: 1,
+        // Player online
+        // Values: 0 - offline | 1 - online
+        on: 0,
         // Damage Protection
+        // Values: 0 - unprotected | 1 - protected
         p: 0,
         // Resources - ToDo
         r: {},
